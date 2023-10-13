89b71441f32a51afe68bd7377ffed4c840bb2e12
freeze0 <44439196+freeze0@users.noreply.github.com>
2023-10-13 09:21:10 +0300

    Update Student.kt

--- app/src/main/java/ru/kubsu/studentsredactor/data/Student.kt
+++ app/src/main/java/ru/kubsu/studentsredactor/data/Student.kt
@@ -5,4 +5,10 @@ data class Student(var studentId: Int,
                    var surname: String,
                    var patronymic: String,
                    var group: String,
-                   var course: Int)
+                   var course: Int) {
+
+    override fun toString(): String {
+        return "Имя = '$name' \nФамилия= '$surname' \n" +
+                "Отчество = '$patronymic' \nГруппа = '$group' \nКурс= $course"
+    }
+}
